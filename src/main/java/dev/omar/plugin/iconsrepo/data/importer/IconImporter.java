79fbb0bd0f3d035ccc52eb714a7e05921677ccd9
omar-haidar <rovox2000@gmail.com>
2026-05-13 13:13:55 +0300

    Update IconImporter function

--- src/main/java/dev/omar/plugin/iconsrepo/data/importer/IconImporter.java
+++ src/main/java/dev/omar/plugin/iconsrepo/data/importer/IconImporter.java
@@ -1,5 +1,7 @@
 package dev.omar.plugin.iconsrepo.data.importer;
 
+import java.io.OutputStream;
+
 public interface IconImporter {
-    ImportIconResult importIcon(IconSource source);
+    ImportIconResult importIcon(IconSource source, OutputStream outputStream);
 }
